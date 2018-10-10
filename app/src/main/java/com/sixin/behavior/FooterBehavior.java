0e9608777ab6c4012adf1e2829dd9fb17f3273e7
cutylifestyle <2055305346@qq.com>
2018-10-10 22:09:58 +0800

    04

--- app/src/main/java/com/sixin/behavior/FooterBehavior.java
+++ app/src/main/java/com/sixin/behavior/FooterBehavior.java
@@ -3,6 +3,7 @@ package com.sixin.behavior;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -36,8 +37,14 @@ public class FooterBehavior extends CoordinatorLayout.Behavior {
         return super.onDependentViewChanged(parent, child, dependency);
     }
 
-    //在按下的时候会触发该方法
-    //TODO 这个方法的返回值的作用是什么
+    //在按下的时候会触发该方法，这个方法的作用其实是确定在一次事件流中该组件是否参与嵌套滑动
+    //这个方法的返回值的作用是什么:对于与它同级的AppBarLayout而言，按下时候的返回值始终是true,
+    // 会最终导致NestedScrollingChildHelper中的判断始终是true,也就是说CoordinateLayout中的
+    //onNestedScrollAccepted该方法会始终被调用。但是注意了对于behavior的onStartNestedScroll
+    //的放回值直接作用的对象是LayoutParams中的方法setNestedScrollAccepted。该方法的设置结果
+    //会影响到LayoutParams中isNestedScrollAccepted方法的结果，而一个Behavior中的与滑动相关的
+    //方法的调用之前都会去验证这个方法是否返回true，如果不是，那么后续方法是得不到调用的，也就是
+    //无法实现嵌套滑动
     @Override
     public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
         Log.d(TAG, "child:" + child.getClass().getName()+"\n"
@@ -45,12 +52,16 @@ public class FooterBehavior extends CoordinatorLayout.Behavior {
                 + "target:"+target.getClass().getName()+"\n"
                 +"axes:"+axes+"\n"
                 +"type:"+type);
-        return super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, axes, type);
+        return (axes & ViewCompat.SCROLL_AXIS_VERTICAL)!=0;
     }
 
-    //TODO 在CoordinatorLayout中与之相关方法的调用的作用是什么
+    //在CoordinatorLayout中与之相关方法的调用的作用是什么?
+    //该方法被回调的前提是onStartNestedScroll放回值为true.能够到达这个方法说明该View参与本次滑动事件流
+    //在AppBarLayout中的Behavior中并没有实现下列方法。其实这个方法的作用就是在本次滑动事件开始前做一些
+    //准备工作可以实现这个方法
+    //TODO 复写recyclerView的Behavior，观察在初始的事件流中下列方法有没有被回调
     @Override
     public void onNestedScrollAccepted(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
-        super.onNestedScrollAccepted(coordinatorLayout, child, directTargetChild, target, axes, type);
+        Log.d(TAG, "onNestedScrollAccepted");
     }
 }

bc1e12f1d72dae1c77bcf9bdf2e757a1f1cf3904
CUTYlife <2055305346@qq.com>
2018-10-11 10:02:04 +0800

    06

--- app/src/main/java/com/sixin/behavior/FooterBehavior.java
+++ app/src/main/java/com/sixin/behavior/FooterBehavior.java
@@ -2,6 +2,7 @@ package com.sixin.behavior;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
@@ -27,14 +28,23 @@ public class FooterBehavior extends CoordinatorLayout.Behavior {
     @Override
     public boolean layoutDependsOn(@NonNull CoordinatorLayout parent, @NonNull View child, @NonNull View dependency) {
         Log.d(TAG, "child:" + child.getClass().getName() + " dependency:" + dependency.getClass().getName());
-        return super.layoutDependsOn(parent, child, dependency);
+        return false;
     }
 
-    //1:onChildViewsChanged   dispatchDependentViewsChanged  offsetChildToAnchor
-    //TODO 暂且不去研究
+    //在这个组合中，调用联合滑动的方法onNestedPreScroll方法，对于AppBar而言
+    //,通过了isNestedScrollAccepted的判断，调用了它的behavior的方法onNestedPreScroll
+    //实现AppBar滑动，对于recyclerView,没有通过isNestedScrollAccepted的判断，
+    //它的behavior的onNestedPreScroll方法没有被调用。但是他却实现了联合滑动，
+    //为什么呢？那是因为代码走到了CoordinateLayout的1360行，走到这行代码的前提
+    //条件需要view之间有依赖关系，recyclerView的Behavior实现了该方法，实现联动
+    //效果的,但是重写这个方法不推荐
     @Override
     public boolean onDependentViewChanged(@NonNull CoordinatorLayout parent, @NonNull View child, @NonNull View dependency) {
-        return super.onDependentViewChanged(parent, child, dependency);
+//        Log.d(TAG, "onDependentViewChanged------:"+"child:"+child.getClass().getName()+"\n"+"dependency:"+dependency.getClass().getName());
+//        int y = dependency.getTop();
+//        Log.d(TAG, "y-------------:" + y);
+//        child.setTranslationY(-y);
+        return false;
     }
 
     //在按下的时候会触发该方法，这个方法的作用其实是确定在一次事件流中该组件是否参与嵌套滑动
@@ -59,9 +69,20 @@ public class FooterBehavior extends CoordinatorLayout.Behavior {
     //该方法被回调的前提是onStartNestedScroll放回值为true.能够到达这个方法说明该View参与本次滑动事件流
     //在AppBarLayout中的Behavior中并没有实现下列方法。其实这个方法的作用就是在本次滑动事件开始前做一些
     //准备工作可以实现这个方法
-    //TODO 复写recyclerView的Behavior，观察在初始的事件流中下列方法有没有被回调
+    //复写recyclerView的Behavior，在初始的事件流中下列方法没有被回调
+    //TODO 对于recyclerView的Behavior,后续的与滑动相关的方法有没有被回调
     @Override
     public void onNestedScrollAccepted(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
         Log.d(TAG, "onNestedScrollAccepted");
+    }
+
+    @Override
+    public void onNestedPreScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View target, int dx, int dy, @NonNull int[] consumed, int type) {
+        Log.d(TAG, "onNestedPreScroll-----------"+"\n"
+                +"child:"+child.getClass().getName()+"\n"
+                +"target:"+target.getClass().getName()+"\n"
+                +"dy:"+dy+"\n"
+                +"dx:"+dx+"\n"
+                +"type:"+type);
     }
 }
---
+++ app/src/main/java/com/sixin/behavior/RecyclerViewBehavior.java
@@ -0,0 +1,43 @@
+package com.sixin.behavior;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+public class RecyclerViewBehavior extends AppBarLayout.ScrollingViewBehavior {
+
+    private static final String TAG = "RecylcerViewBehavior";
+
+    public RecyclerViewBehavior(Context context, AttributeSet attrs) {
+        super(context,attrs);
+    }
+
+    @Override
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
+        return super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, axes, type);
+    }
+
+    //没有被回调
+    @Override
+    public void onNestedScrollAccepted(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
+        super.onNestedScrollAccepted(coordinatorLayout, child, directTargetChild, target, axes, type);
+        Log.d(TAG, "onNestedScrollAccepted");
+    }
+
+    //没有被回调
+    @Override
+    public void onNestedPreScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View target, int dx, int dy, @NonNull int[] consumed, int type) {
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type);
+        Log.d(TAG, "onNestedPreScroll");
+    }
+
+    //被回调了
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
+        return super.onDependentViewChanged(parent, child, dependency);
+    }
+}

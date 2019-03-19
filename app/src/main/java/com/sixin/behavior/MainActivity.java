a9d0b1a3e32195dd2b78b99557576c4f9ccbffc8
cutylifestyle <2055305346@qq.com>
2019-03-19 10:00:49 +0800

    master---->01

--- app/src/main/java/com/sixin/behavior/MainActivity.java
+++ app/src/main/java/com/sixin/behavior/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String a = "hello";
         mRecyclerView = findViewById(R.id.recyclerView);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.HORIZONTAL));
